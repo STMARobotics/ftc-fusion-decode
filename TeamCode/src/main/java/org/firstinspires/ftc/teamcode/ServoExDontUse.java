54d07b9b9850db3eaa5ae04a9b1aeaf704f75bcd
molliemur <mollie.muraski@stmarobotics.org>
2025-09-10 18:41:47 -0500

    made a new branch :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoExDontUse.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class ServoExDontUse {
-    public ServoExDontUse(String clawServo) {
-    }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetPositionDontUse.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class SetPositionDontUse {
-    public SetPositionDontUse(ServoExDontUse servo, double v) {
-    }
-
-    public Command requires(Subsystem.Claw claw) {
-    }
-}
